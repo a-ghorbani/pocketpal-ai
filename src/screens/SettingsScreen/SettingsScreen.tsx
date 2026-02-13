c8568b540dc855fa91c36df442887eb5ea4adc73
a-ghorbani <ghorbani59@gmail.com>
2026-02-13 13:39:36 +0100

    feat(l10n): wire Indonesian (id) language into the app

--- src/screens/SettingsScreen/SettingsScreen.tsx
+++ src/screens/SettingsScreen/SettingsScreen.tsx
@@ -70,6 +70,7 @@ const languageNames: Record<AvailableLanguage, string> = {
   en: 'English (EN)',
   //es: 'Español (ES)',
   //de: 'Deutsch (DE)',
+  id: 'Bahasa Indonesia (ID)',
   ja: '日本語 (JA)',
   //ko: '한국어 (KO)',
   //pl: 'Polski (PL)',
