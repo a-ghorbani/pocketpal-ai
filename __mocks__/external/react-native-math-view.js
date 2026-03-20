cc1909793837593404c4d12da9c7361a48f2a9c1
1234 <dddu809@gmail.com>
2026-03-20 20:18:56 +0000

    Update react-native-math-view.js

--- __mocks__/external/react-native-math-view.js
+++ __mocks__/external/react-native-math-view.js
@@ -5,10 +5,11 @@ const MathView = ({math, ...props}) => {
   return React.createElement(Text, props, math);
 };
 
-const mockSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10"></svg>';
+const mockSvg =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10"></svg>';
 
 export const MathjaxFactory = () => {
-  const toSVG = (math) => ({
+  const toSVG = math => ({
     svg: mockSvg,
     size: {width: 100, height: 40},
   });
